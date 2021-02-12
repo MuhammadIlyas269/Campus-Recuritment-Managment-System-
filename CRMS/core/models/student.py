00acec395fd30f94328317d73a6c83e31b8940d2
MuhammadIlyas269 <MuhammadIlyas523221@gmail.com>
2021-02-13 01:22:36 +0500

    fix a student model stud_id issue

--- CRMS/core/models/student.py
+++ CRMS/core/models/student.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 class Student(models.Model):
+    stud_id = models.CharField(max_length=25, null=True, blank=False)
     name = models.CharField(max_length=50)
     about_me = models.TextField(blank=False)
     email = models.EmailField(max_length=254)
+    profile_pic = models.ImageField(upload_to="images/", null=True, blank=False)
     
--- CRMS/core/urls.py
+++ CRMS/core/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
 from .views import index
+from django.conf import settings 
+from django.conf.urls.static import static 
+
+#create urls here 
+
 urlpatterns = [
     path('', index),
 ]
+if settings.DEBUG: 
+        urlpatterns += static(settings.MEDIA_URL, 
+                              document_root=settings.MEDIA_ROOT)
