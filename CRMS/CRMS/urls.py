54d09aa4a856e6174108e68d07187c514385d630
Ilyas <MuhammadIlyas523221@gmail.comgit config --global user.name Ilyasexitgit config --global user.email MuhammadIlyas523221@gmail.com>
2021-03-09 21:24:18 +0500

    Refactors Urls.py

--- CRMS/CRMS/urls.py
+++ CRMS/CRMS/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from os import name
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views as auth_views
+
 
 
 
@@ -23,4 +25,24 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls,),
     path('core/',include('core.urls')),
+     ######### Login and Logout #####################
+    path('accounts/login/', auth_views.LoginView.as_view(),name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(),name='logout'),
+
+    ########## Password reset urls ##################
+    path('accounts/password-reset/',auth_views.PasswordResetView.as_view(
+             template_name='registration/password/password_reset_form.html',
+             subject_template_name='registration/password/password_reset_subject.txt',
+             email_template_name='registration/password/password_reset_email.html',
+             success_url='/core/accounts/password-reset/done/'),name='password_reset'),
+   
+    path('accounts/password-reset/done/',auth_views.PasswordResetDoneView.as_view(
+             template_name='registration/password/password_reset_done.html'),name='password_reset_done'),
+   
+    path('accounts/password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+             template_name='registration/password/password_reset_confirm.html',
+             success_url='/core/accounts/password-reset-complete/' ),name='password_reset_confirm'),
+    
+    path('accounts/password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+             template_name='registration/password/password_reset_complete.html' ),name='password_reset_complete'),
 ]
--- CRMS/core/urls.py
+++ CRMS/core/urls.py
@@ -18,55 +18,37 @@ urlpatterns = [
 
     # path('accounts/', include('django.contrib.auth.urls')),
 
-    ######### Login and Logout #####################
-    path('accounts/login/', auth_views.LoginView.as_view(),name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(),name='logout'),
+   
 
     ########### Rediret View after Successfull Login #############################
     path('redirect/', REDIRECT_VIEW, name='REDIRECT_VIEW'), 
    
    
-    ########## Password reset urls ##################
-    path('accounts/password-reset/',auth_views.PasswordResetView.as_view(
-             template_name='registration/password/password_reset_form.html',
-             subject_template_name='registration/password/password_reset_subject.txt',
-             email_template_name='registration/password/password_reset_email.html',
-             success_url='/core/accounts/password-reset/done/'),name='password_reset'),
-   
-    path('accounts/password-reset/done/',auth_views.PasswordResetDoneView.as_view(
-             template_name='registration/password/password_reset_done.html'),name='password_reset_done'),
-   
-    path('accounts/password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-             template_name='registration/password/password_reset_confirm.html',
-             success_url='/core/accounts/password-reset-complete/' ),name='password_reset_confirm'),
-    
-    path('accounts/password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
-             template_name='registration/password/password_reset_complete.html' ),name='password_reset_complete'),
-
-
+    ################ Signup request urls ###############
+    path('signup/company/',CompanySignupRequestView.as_view(), name = 'company_signup_request'),
+    path('signup/student/',StudentSignupRequestView.as_view(), name = 'student_signup_request'),
 
     ############ Signup Form ################
     path('accounts/signup/', SignupView.as_view(), name='signup'),
     path('accounts/signup/success/', TemplateView.as_view(template_name='success.html'), name='success'),
     
     path('accounts/signup/student/', StudentSignupView.as_view(), name='student_signup'),
-    
     path('accounts/signup/company/', CompanySignupView.as_view(), name='company_signup'),
     
     
-    #uers Page + admin panel
+    ########### Student urls #####
     path('accounts/student/me/',StudentPage.as_view(), name='student_page'),
 
+    ####### Comapny urls #####    
     path('accounts/company/me/',CompanyPage.as_view(), name='company_page'),
     path('accounts/company/profile/',CompanyProfileView.as_view(), name='company_profile'),
     path('accounts/company/me/createJob/',CreateJobPostView.as_view(), name='create_job'),
 
-    path('accounts/admin/me/',ModratorPage.as_view(), name='modrator_page'),
+    ####### Admin urls #####
     path('admin/', admin.site.urls, name='admin_page'),
+    path('accounts/admin/me/',ModratorPage.as_view(), name='modrator_page'),
     
-    #signup request urls
-    path('signup/company/',CompanySignupRequestView.as_view(), name = 'company_signup_request'),
-    path('signup/student/',StudentSignupRequestView.as_view(), name = 'student_signup_request'),
+   
 
 
 
